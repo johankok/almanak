14293129779a9c8a3942674928f5cfe7bf4d3cc5
Johan Kok <info@johankok.nl>
2014-03-17 21:11:56 +0100

    add aantalInwoners and totaalZetels

--- viewer.rb
+++ viewer.rb
@@ -11,7 +11,8 @@ file_contents = File.read('fixtures/20140221220000.xml')
 
 CSV.open("gemeenten.csv", "wb") do |csv|
   collection = Almanak::Overheidsorganisaties.parse(file_contents)
+  csv << ["gemeente.naam", "gemeente.afkorting", "gemeente.provincie", "gemeente.contact.telefoon", "gemeente.contact.emailadres", "gemeente.contact.internet", "gemeente.aantalInwoners", "gemeente.totaalZetels"]
   collection.gemeenten.each do |gemeente|
-    csv << [gemeente.naam, gemeente.afkorting, gemeente.provincie, gemeente.contact.telefoon, gemeente.contact.emailadres, gemeente.contact.internet]
+    csv << [gemeente.naam, gemeente.afkorting, gemeente.provincie, gemeente.contact.telefoon, gemeente.contact.emailadres, gemeente.contact.internet, gemeente.aantalInwoners, gemeente.totaalZetels]
   end
 end
